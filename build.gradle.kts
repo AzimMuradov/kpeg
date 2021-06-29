289a03fbfe049bcebb5c3a5c70cf6d2980cd0d92
Azim Muradov <azim.muradov.dev@gmail.com>
2021-06-30 00:57:20 +0300

    Add `dokka` to the build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    kotlin("jvm") version "1.5.20" apply false
+
+    id("org.jetbrains.dokka") version "1.4.32" apply false
+}
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,9 +1,14 @@
+import java.net.URL
+
+
 plugins {
-    kotlin("jvm") version "1.5.20"
+    kotlin("jvm")
 
     `java-library`
 
     jacoco
+
+    id("org.jetbrains.dokka")
 }
 
 repositories {
@@ -52,4 +57,73 @@ tasks {
 
 jacoco {
     toolVersion = "0.8.7"
+}
+
+
+// Docs configuration
+
+tasks {
+    dokkaHtml {
+        outputDirectory.set(buildDir.resolve("dokka"))
+
+        // Set module name displayed in the final output
+        moduleName.set("kpeg library")
+
+        dokkaSourceSets {
+            configureEach {
+
+                // Use to include or exclude non public members
+                includeNonPublic.set(false)
+
+                // Do not output deprecated members. Applies globally, can be overridden by packageOptions
+                skipDeprecated.set(true)
+
+                // Emit warnings about not documented members. Applies globally, also can be overridden by packageOptions
+                reportUndocumented.set(true)
+
+                // Do not create index pages for empty packages
+                skipEmptyPackages.set(true)
+
+                // This name will be shown in the final output
+                displayName.set("JVM")
+
+                // Platform used for code analysis. See the "Platforms" section of this readme
+                platform.set(org.jetbrains.dokka.Platform.jvm)
+
+                // List of files with module and package documentation
+                // https://kotlinlang.org/docs/reference/kotlin-doc.html#module-and-package-documentation
+                // includes.from("packages.md", "extra.md")
+
+                // List of files or directories containing sample code (referenced with @sample tags)
+                // samples.from("samples/basic.kt", "samples/advanced.kt")
+
+                // Specifies the location of the project source code on the Web.
+                // If provided, Dokka generates "source" links for each declaration.
+                // Repeat for multiple mappings
+                sourceLink {
+                    // Unix based directory relative path to the root of the project (where you execute gradle respectively).
+                    localDirectory.set(file("src/main/kotlin"))
+
+                    // URL showing where the source code can be accessed through the web browser
+                    remoteUrl.set(URL("https://github.com/AzimMuradov/kpeg/blob/dev/lib/src/main/kotlin"))
+                    // Suffix which is used to append the line number to the URL. Use #L for GitHub
+                    remoteLineSuffix.set("#L")
+                }
+
+                // Used for linking to JDK documentation
+                jdkVersion.set(15)
+
+                // Disable linking to online kotlin-stdlib documentation
+                noStdlibLink.set(false)
+
+                // Disable linking to online JDK documentation
+                noJdkLink.set(false)
+            }
+        }
+
+        doLast {
+            println("View docs at:")
+            println("file://$buildDir/dokka/index.html")
+        }
+    }
 }
