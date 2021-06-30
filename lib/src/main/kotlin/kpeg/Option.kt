b87120f848c2b6f5c37cf1664fc605463227d514
Azim Muradov <azim.muradov.dev@gmail.com>
2021-07-01 01:01:31 +0300

    Add docs to Option.kt, make it ext. functions internal

--- lib/src/main/kotlin/kpeg/Option.kt
+++ lib/src/main/kotlin/kpeg/Option.kt
@@ -4,10 +4,22 @@ import kpeg.Option.None
 import kpeg.Option.Some
 
 
+// Soon would be replaced with the Option class of the Λrrow library
+// See: https://arrow-kt.io/
+
+/**
+ * Option - it either [Some(value)][Some] or [None].
+ */
 public sealed class Option<out T> {
 
+    /**
+     * Some represents [Option] that holds [value] of type [T].
+     */
     public data class Some<T>(public val value: T) : Option<T>()
 
+    /**
+     * None represents [Option] that holds nothing.
+     */
     public object None : Option<Nothing>() {
 
         override fun toString(): String = "None"
@@ -15,23 +27,22 @@ public sealed class Option<out T> {
 }
 
 
-public fun <T> Option<T>.unwrapOrNull(): T? = when (this) {
+internal fun <T> Option<T>.unwrapOrNull(): T? = when (this) {
     is Some -> value
     None -> null
 }
 
-public fun <T> Option<T>.unwrap(): T = when (this) {
+internal fun <T> Option<T>.unwrap(): T = when (this) {
     is Some -> value
     None -> throw IllegalStateException("Option is None")
 }
-
 
 internal fun <T> T.takeAsOptionIf(condition: (T) -> Boolean): Option<T> = when (condition(this)) {
     true -> Some(this)
     false -> None
 }
 
-public inline fun <T> Option<T>.alsoIfSome(block: (T) -> Unit): Option<T> = when (this) {
+internal inline fun <T> Option<T>.alsoIfSome(block: (T) -> Unit): Option<T> = when (this) {
     is Some -> Some(value.also(block))
     None -> this
 }
