ff2af5a1d5e3109cd3aa791fa3dceb9e5daa016c
Azim Muradov <azim.muradov.dev@gmail.com>
2021-07-01 00:55:57 +0300

    Add `list` and `joinToString` operators

--- lib/src/main/kotlin/kpeg/pe/Operators.kt
+++ lib/src/main/kotlin/kpeg/pe/Operators.kt
@@ -62,6 +62,15 @@ public sealed class Operators {
 
     public fun literal(l: String): PE<String> = Literal(l.length) { it == l }
 
+    public fun <T> PE<List<T>>.joinToString(
+        separator: CharSequence = "",
+        prefix: CharSequence = "",
+        postfix: CharSequence = "",
+        limit: Int = -1,
+        truncated: CharSequence = "...",
+        transform: ((T) -> CharSequence)? = null,
+    ): PE<String> = Map({ it.joinToString(separator, prefix, postfix, limit, truncated, transform) }, pe = this)
+
 
     // Optional
 
@@ -83,6 +92,51 @@ public sealed class Operators {
     public fun <T> PE<T>.zeroOrMore(): PE<List<T>> = Repeated(range = 0u..UInt.MAX_VALUE, pe = this)
 
     public fun <T> PE<T>.oneOrMore(): PE<List<T>> = Repeated(range = 1u..UInt.MAX_VALUE, pe = this)
+
+    public fun <T> PE<T>.list(
+        separator: PE<*>? = null,
+        prefix: PE<*>? = null,
+        postfix: PE<*>? = null,
+        min: UInt = 0u, max: UInt = UInt.MAX_VALUE,
+    ): PE<List<T>> =
+        if (min <= max) {
+            Group.Sequence {
+                prefix?.unaryPlus()
+
+                val content =
+                    if (min == 0u && max > 0u) {
+                        +Group.Sequence<List<T>> {
+                            val first = +this@list
+                            val others = +Repeated(range = 0u..(max - 1u), pe = Group.Sequence<T> {
+                                separator?.unaryPlus()
+                                val next = +this@list
+                                value { next.get }
+                            })
+
+                            value { listOf(first.get) + others.get }
+                        }.orDefault(emptyList())
+                    } else if (min > 0u && max > 0u) {
+                        +Group.Sequence<List<T>> {
+                            val first = +this@list
+                            val others = +Repeated(range = (min - 1u)..(max - 1u), pe = Group.Sequence<T> {
+                                separator?.unaryPlus()
+                                val next = +this@list
+                                value { next.get }
+                            })
+
+                            value { listOf(first.get) + others.get }
+                        }
+                    } else {
+                        +Empty.map { emptyList<T>() }
+                    }
+
+                postfix?.unaryPlus()
+
+                value { content.get }
+            }
+        } else {
+            Fail
+        }
 
 
     // Predicate
