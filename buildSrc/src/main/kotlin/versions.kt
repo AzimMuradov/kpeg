7fe81484d8532c3f56c95d61967e07fc959168f5
Azim Muradov <azim.muradov.dev@gmail.com>
2021-08-08 01:14:13 +0300

    Update versions

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -1,7 +1,7 @@
 object V {
 
     // Plugins
-    const val P_KOTLIN: String = "1.5.20"
+    const val P_KOTLIN: String = "1.5.21"
     const val P_DOKKA: String = "1.5.0"
     const val P_NEXUS_PUBLISH: String = "1.1.0"
 
@@ -10,7 +10,7 @@ object V {
 
     // For test
     const val JUPITER: String = "5.7.2"
-    const val KOTEST_ASSERTIONS_CORE: String = "4.6.0"
+    const val KOTEST_ASSERTIONS_CORE: String = "4.6.1"
     const val KOTEST_ASSERTIONS_ARROW: String = "1.0.2"
     const val DISCRETE_MATH_TOOLKIT: String = "1.0.3"
 
